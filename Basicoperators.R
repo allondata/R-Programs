cff7295121f38c08050fd020bbda7395fbb4acff
James basker <jamesxbasker@gmail.com>
2018-07-04 10:02:29 +0530

    Using Basic operators +, -, *, /

---
+++ Basicoperators.R
@@ -0,0 +1,11 @@
+# This is a comment, comment always starts with #
+# Author: http://learnerstop.com
+# Version used: 3.5.1
+
+# Using Basic operators
+
+print(5 + 3)
+print(5 - 4)
+print(5 * 3)
+print(5 / 3)
+
